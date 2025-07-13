ae5781f7d6fe9d075a79a7d9e5726ebab4d606aa
Cristian Hernandez <cristianhernandez301205@gmail.com>
2025-07-13 11:01:43 -0500

    mejoras

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-      <Router>
-        <Routes>
-        </Routes>
-      </Router>
+
   );
 }
 

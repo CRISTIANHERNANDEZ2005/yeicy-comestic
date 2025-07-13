8f64451fbdcc5e6b420a9ba3c09f04cd38e20419
Cristian Hernandez <cristianhernandez301205@gmail.com>
2025-07-13 11:08:47 -0500

    mejoras

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/publico/Landing';
+import Home from './pages/cliente/Home';
+import Dashboard from './pages/admin/Dashboard';
+import NotFound from './pages/error/NotFound';
 
 function App() {
   return (
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/cliente" element={<Home />} />
+        <Route path="/admin" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
